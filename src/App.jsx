044a7340b3d1a9b548541bfdfe1130959aaa93fb
chethank <chethan.kumar@myn.global>
2024-06-25 22:06:56 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,28 @@ import { listContext } from "./context/listContext";
 import { Tabs } from "./components/Tab";
 import Product from "./components/Product";
 function App() {
-  //const { list } = useContext(listContext);
+  const { list } = useContext(listContext);
   const { index, setIndex } = useContext(listContext);
 
   return (
     <>
       {/* <Recursive fileExplorer={fileExplorer} /> */}
-      {/*   <Select>
+      <Select>
         <Select.Option value="apple">Apple</Select.Option>
         <Select.Option value="banana">banana</Select.Option>
         <Select.Option value="city">city</Select.Option>
         <Select.Option value="apple">Apple</Select.Option>
       </Select>
-      <button onClick={() => console.log(list)}>Get selected</button> */}
+      <button onClick={() => console.log(list)}>Get selected</button>
 
-      {/* <Tabs>
+      <Tabs>
         <Tabs.Tab activeIndex={1}>Tab1</Tabs.Tab>
         <Tabs.Tab activeIndex={2}>Tab2</Tabs.Tab>
         <Tabs.Tab activeIndex={3}>Tab3</Tabs.Tab>
 
         <Tabs.TabPanel passedIndex={1}>
           <Tabs.TabHeader>Header 1</Tabs.TabHeader>
+          <Tabs.TabContent>Content 1</Tabs.TabContent>
         </Tabs.TabPanel>
         <Tabs.TabPanel passedIndex={2}>
           <Tabs.TabHeader>Header 2</Tabs.TabHeader>
@@ -35,7 +36,7 @@ function App() {
           <Tabs.TabHeader>Header 3</Tabs.TabHeader>
           <Tabs.TabContent>content 3</Tabs.TabContent>
         </Tabs.TabPanel>
-      </Tabs> */}
+      </Tabs>
 
       {/* <Product>
         <Product.ProductHeader>Product Engineer</Product.ProductHeader>
